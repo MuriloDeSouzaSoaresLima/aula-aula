65eb10f1c6361a6811267df0c1813653b0660dca
murilo.lima <murilossl07@gmail.com>
2025-03-07 15:52:57 -0300

    alunoA inserindo linha 3, 4 e 5

--- conflito.ts
+++ conflito.ts
@@ -1,2 +1,5 @@
 console.log('alunoA alterou a linha1 do arquivo conflito.ts');
 console.log('alunoB inserindo a linha2');
+
+let concatenar: string = 'Git' + 'Hub';
+console.log(`concatenando ${concatenar}`);
